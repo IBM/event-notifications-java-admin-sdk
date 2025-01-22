ce7a63a29a2c04427bed6d6b74344c638104a8ec
nitish <nitish.kulkarni3@ibm.com>
2025-01-22 14:20:36 +0530

    chore: code update for workflow
    nitish <nitish.kulkarni3@ibm.com>

--- build/setMavenVersion.sh
+++ build/setMavenVersion.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-# This script will check $TRAVIS_TAG to see if we need to run maven to
+# This script will check $GITHUB_REF to see if we need to run maven to
 # set the artifact version #'s.
 
-if [[ -n "${TRAVIS_TAG}" ]]; then
-    printf "\n>>>>> Setting artifact version #'s to: %s\n" ${TRAVIS_TAG}
-    mvn versions:set -DnewVersion=${TRAVIS_TAG} -DgenerateBackupPoms=false
+if [[ $GITHUB_REF == refs/tags/* ]]; then
+    VERSION=${GITHUB_REF#refs/tags/}
+    printf "\n>>>>> Setting artifact version #'s to: %s\n" ${VERSION}
+    mvn versions:set -DnewVersion=${VERSION} -DgenerateBackupPoms=false
 else
     printf "\n>>>>> Bypassing artifact version setting for non-tagged build\n"
 fi
-     
